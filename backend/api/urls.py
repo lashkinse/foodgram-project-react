f96e9941bda5c5208c8bcce3532b8a2f902edf0f
Lashkin Sergey <lashkinse@yandex.ru>
2023-05-14 23:36:30 +0400

    added missed CustomUserViewSet url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import TagViewSet, IngredientViewSet
+from api.views import TagViewSet, IngredientViewSet, CustomUserViewSet
 
 router = DefaultRouter()
 router.register("tags", TagViewSet, basename="tags")
 router.register("ingredients", IngredientViewSet, basename="ingredients")
+router.register("users", CustomUserViewSet, basename="users")
 
 auth_patterns = [
     path("auth/", include("djoser.urls.authtoken")),
