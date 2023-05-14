eb948fccae0bda4494b84c69c40a4c260d40d153
Lashkin Sergey <lashkinse@yandex.ru>
2023-05-15 00:13:20 +0400

    added me action

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,13 +4,13 @@ from rest_framework.routers import DefaultRouter
 from api.views import TagViewSet, IngredientViewSet, CustomUserViewSet
 
 router = DefaultRouter()
+router.register("users", CustomUserViewSet, basename="users")
 router.register("tags", TagViewSet, basename="tags")
 router.register("ingredients", IngredientViewSet, basename="ingredients")
-router.register("users", CustomUserViewSet, basename="users")
 
 auth_patterns = [
-    path("auth/", include("djoser.urls.authtoken")),
     path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]
 
 urlpatterns = [
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.filters import IngredientFilter
 from api.mixins import ListRetrieveModelMixin
+from api.permissions import IsAuthenticated
 from api.serializers import (
     TagSerializer,
     IngredientSerializer,
@@ -18,6 +21,15 @@ class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     http_method_names = ["get", "post", "delete"]
+
+    @action(
+        detail=False, methods=["GET"], permission_classes=[IsAuthenticated]
+    )
+    def me(self, request):
+        serializer = CustomUserSerializer(
+            request.user, context={"request": request}
+        )
+        return Response(serializer.data)
 
 
 class TagViewSet(ListRetrieveModelMixin):
